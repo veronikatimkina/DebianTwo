111bee2098e39f43a4b8642c9c59b944ad444cd1
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-06-01 16:21:42 +0300

    Change Class3 from Clone1

--- src/package1/Class3.java
+++ src/package1/Class3.java
@@ -3,6 +3,6 @@ package package1;
 public class Class3 {
     public static void main(String[] args) {
         int i = 1223;
-        System.out.println(i);
+        System.out.println(i+1);
     }
 }
