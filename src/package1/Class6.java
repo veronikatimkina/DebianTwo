9102696ecec264ecd0ef3474e85898300992df56
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-06-01 17:41:25 +0300

    Change Class6 in Branch1 from Clone1

--- src/package1/Class6.java
+++ src/package1/Class6.java
@@ -2,6 +2,6 @@ package package1;
 
 public class Class6 {
     public static void main(String[] args) {
-        System.out.println("qwqwqw");
+        System.out.println("uvcvuwevcyuwec");
     }
 }
