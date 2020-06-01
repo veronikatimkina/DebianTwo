b87fc7698008b977ea2f14ced625d83d7929bc3d
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-06-01 19:59:27 +0300

    commit from terminal

--- src/package1/Class3.java
+++ src/package1/Class3.java
@@ -3,7 +3,7 @@ package package1;
 public class Class3 {
     public static void main(String[] args) {
         int i = 1223;
-        int j = 90;
+        int j = 111;
         System.out.println(i+j);
     }
 }
