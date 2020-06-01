d0be7d974ae46f206c9f3ef6983c0e62ddd11028
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-06-01 20:04:38 +0300

    Edit Class3 in master from Clone1

--- src/package1/Class3.java
+++ src/package1/Class3.java
@@ -3,7 +3,7 @@ package package1;
 public class Class3 {
     public static void main(String[] args) {
         int i = 1223;
-        int j = 111;
+        int j = 1110;
         System.out.println(i+j);
     }
 }
